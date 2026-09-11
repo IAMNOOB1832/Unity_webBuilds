2c85020b24a6fc6b2ed48358f596a51dac580599
IAMNOOB1832 <lucas.knaapen@live.nl>
2026-09-11 11:33:47 +0200

    supabase.js updated

--- supabase.js
+++ supabase.js
@@ -1,2 +1,11 @@
 const SUPABASE_URL = "https://rfbsjpghlhcxqesvftta.supabase.co";
 const SUPABASE_KEY = "sb_publishable_nmldYMV_7dgG375ZpnT9gA_ncPSwV2I";
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
+
+window.supabaseClient =
+    supabaseClient;
